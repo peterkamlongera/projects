69d3fd84123ad4d5fe2652c4c13b5906de000e3c
Peter <peterkamlongera@gmail.com>
2023-03-25 22:56:14 +0200

    added add functionality, needs to allow > 1 input

--- level-1/todo-app/main.js
+++ level-1/todo-app/main.js
@@ -1,49 +1,24 @@
-// Selectors
-const newTaskInput = document.getElementById("new-task-input");
-const addTaskBtn = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+// Query Selectors
+const input = document.getElementById("to-do-task");
+const addItem = document.getElementById("to-do-list");
+const appendItem = document.getElementById("to-do-list-container");
+const removeItem = document.getElementById("to-do-list-item");
+const newListItem = document.createElement("li")
 
-// Event listeners
-addTaskBtn.addEventListener("click", addTask);
-todoList.addEventListener("click", handleListClick);
+// let li = document.createElement("li")
 
-// Functions
-function addTask() {
-  // Get the value of the input
-  const taskText = newTaskInput.value;
+// Event Listeners
+addItem.addEventListener("click", newTask());
+appendItem.addEventListener("click", newTask());
+removeItem.addEventListener("click", removeTask());
 
-  // Create a new list item
-  const newListItem = document.createElement("li");
-  newListItem.innerText = taskText;
-
-  // Add a class to the new list item
-  newListItem.classList.add("todo-item");
-
-  // Create a checkbox
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.classList.add("checkbox");
-
-  // Append the checkbox and the new list item to the todo list
-  newListItem.prepend(checkbox);
-  todoList.appendChild(newListItem);
-
-  // Clear the input
-  newTaskInput.value = "";
+// OnClick Functions
+function newTask() {
+   appendItem.appendChild(newListItem);
+   newListItem.textContent = input.value;
 }
 
-function handleListClick(event) {
-  const clickedElement = event.target;
+function deleteTask() {
 
-  // If the clicked element is a checkbox, toggle the class on the parent element
-  if (clickedElement.classList.contains("checkbox")) {
-    const listItem = clickedElement.parentElement;
-    listItem.classList.toggle("completed");
-  }
-
-  // If the clicked element is a delete button, remove the parent element from the todo list
-  if (clickedElement.classList.contains("delete-btn")) {
-    const listItem = clickedElement.parentElement;
-    todoList.removeChild(listItem);
-  }
 }
+
