78a6a28c9bf1c2390cc8b1c2613d6972f49b97c5
Peter <peterkamlongera@gmail.com>
2023-03-30 06:33:09 +0200

    basic version complete, will iterate over time

--- level-1/todo-app/main.js
+++ level-1/todo-app/main.js
@@ -3,9 +3,7 @@ const input = document.getElementById("to-do-task");
 const addItem = document.getElementById("to-do-list");
 const appendItem = document.getElementById("to-do-list-container");
 const removeItem = document.getElementById("to-do-list-item");
-const newListItem = document.createElement("li")
-
-// let li = document.createElement("li")
+const deleteItem = document.deleteItem("li")
 
 // Event Listeners
 addItem.addEventListener("click", newTask());
@@ -14,11 +12,11 @@ removeItem.addEventListener("click", removeTask());
 
 // OnClick Functions
 function newTask() {
+   var newListItem = document.createElement("li")
+   newListItem.innerText = input.value;
    appendItem.appendChild(newListItem);
-   newListItem.textContent = input.value;
 }
 
 function deleteTask() {
-
+   deleteItem
 }
-
