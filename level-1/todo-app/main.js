1f2cfae6b7920573bb2c5608b8a0b5ea3fb15099
Peter <peterkamlongera@gmail.com>
2023-03-30 09:28:45 +0200

    Strugglin with remove items feature

--- level-1/todo-app/main.js
+++ level-1/todo-app/main.js
@@ -3,7 +3,6 @@ const input = document.getElementById("to-do-task");
 const addItem = document.getElementById("to-do-list");
 const appendItem = document.getElementById("to-do-list-container");
 const removeItem = document.getElementById("to-do-list-item");
-const deleteItem = document.deleteItem("li")
 
 // Event Listeners
 addItem.addEventListener("click", newTask());
@@ -12,11 +11,20 @@ removeItem.addEventListener("click", removeTask());
 
 // OnClick Functions
 function newTask() {
-   var newListItem = document.createElement("li")
+   var newListItem = document.createElement("li");
+   newListItem.setAttribute("id", "tdl");
+   const listItemCheckbox = document.createElement("input");
+   listItemCheckbox.setAttribute("type","checkbox");
    newListItem.innerText = input.value;
-   appendItem.appendChild(newListItem);
+   appendItem.appendChild(newListItem).appendChild(listItemCheckbox).setAttribute("id", "checkbox");
 }
 
 function deleteTask() {
-   deleteItem
+   var getCheckedItems = document.getElementById("checkbox");
+   var listedItems = document.getElementById("tdl");
+   console.log(listedItems);
+
+   if (getCheckedItems.checked) {
+      listedItems.removeChild(getCheckedItems)
+   }
 }
